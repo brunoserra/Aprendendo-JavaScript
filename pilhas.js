891d0b0dc55896b322b829ed5742e6d659bc578e
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-28 13:17:07 -0300

    arrawys testada

--- pilhas.js
+++ pilhas.js
@@ -1,17 +1,32 @@
-//[]]seria um vetor
+//[]]seria um vetor//
+//estrutura para pilha //
 var elementos = [];
 var topo = -1;
 const maximo = 10;
 
-//função push coloca os valore empilhados
+//função push coloca os valore empilhados//
 function push(num) {
     if (topo < maximo) {
         topo = topo + 1;
         elementos[topo] = num;
-    }
+    } else { console.log("pilha esta cheia"); }
 }
 
-} else {
-    console.log("Pilha esta cheia")
+//retirada de elementos//
+//!= refere-se a simbulo deferente//
+//let é uma variavel local//
+function pop() {
+    if (topo != -1) {
+        let num = elementos[topo];
+        topo = topo - 1;
+        return num;
+    } else { console.log("pilha vazia"); }
 }
-}
+
+
+//parte do código que usa a pilha//
+push(10);
+push(20);
+push(30);
+
+console.log(elementos);
