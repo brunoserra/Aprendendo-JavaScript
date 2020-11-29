88fa933ceab5f81f1f8baf5034a6c3a235e1bc86
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-28 21:53:29 -0300

    ordenacao compelta

--- algoritimoordenacao.js
+++ algoritimoordenacao.js
@@ -5,13 +5,15 @@ function ordena() {
     let inicio = 0;
     let fim = 8;
     let tmp;
+    for (vezes = 0; vezes < 8; vezes++) {
 
-    for (pos = inicio; pos < fim - 1; pos++) {
-        if (valores[pos] > valores[pos + 1]) {
-            tmp = valores[pos];
-            valores[pos] = valores[pos + 1];
-            valores[pos + 1] = tmp;
+        for (pos = inicio; pos < fim - 1; pos++) {
+            if (valores[pos] > valores[pos + 1]) {
+                tmp = valores[pos];
+                valores[pos] = valores[pos + 1];
+                valores[pos + 1] = tmp;
 
+            }
         }
     }
 }
