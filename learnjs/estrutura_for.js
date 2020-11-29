f20f8463132290221496fbea79c39014318312c1
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 01:10:07 -0300

    tabuada

--- learnjs/estrutura_for.js
+++ learnjs/estrutura_for.js
@@ -8,3 +8,11 @@ for (var i = 0; i < 15; i++) {
     console.log(i);
 
 }
+
+var numeroSorteado = 10
+var tabuada = 7
+
+for (var i = 0; i < 10; i++) {
+    console.log("valor de" + tabuada + "x" + i + " = " + (tabuada * i));
+
+}
