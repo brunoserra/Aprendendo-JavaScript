2b5a72703364c2c7eace89ff96ab083fc5ae9c13
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 00:53:07 -0300

    case

---
+++ learnjs/switch.js
@@ -0,0 +1,16 @@
+//funçaõ switch dentro tenho casos de teste
+
+var nome = "menino"
+switch (nome) {
+    case "bruno":
+        console.log("legal é voce");
+        break;
+    case "serra":
+        console.log("serve voce");
+        break;
+    default:
+        console.log("uma pena não é pra voce");
+        break
+
+
+}
