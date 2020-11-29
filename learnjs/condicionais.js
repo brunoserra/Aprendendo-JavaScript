f95d91da7102b63d08813e2d7473225b5ef11653
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 00:34:17 -0300

    certo if else if

---
+++ learnjs/condicionais.js
@@ -0,0 +1,10 @@
+// condicionais toamda de decisão
+var nome = "serra"
+if (nome === "bruno") {
+    console.log("legal seu nome é esse");
+} else if (nome === "serra") {
+    console.log("serve voce");
+} else {
+    console.log("não é voce");
+
+}
