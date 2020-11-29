13d27b32b74d8f31a3a1a9c07349e9e2f26a26a9
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-28 23:43:53 -0300

    Update num-operadores.js

--- learnjs/num-operadores.js
+++ learnjs/num-operadores.js
@@ -16,3 +16,13 @@ var segundoValor = 20
 var resultados = primeiroValor - segundoValor;
 console.log(resultados);
 // no javascriopt tudo é variavelnão tem distinção
+//boas praticas
+// usar nome cameCasel minusclae maisucla pra delcarar
+// var deve começar com uma letra
+// colar espaço entre os operadores com
+//identar sempre com 2 espeços
+//termine instrução simples com ponto e virgula
+//abertura de chaves sempre na linha da funçao ou objeto
+//colocar a chave final da função em linha isolada
+//não ultrapassar 80linhas de codigo de de comandos de
+//sempre utilize arquivos externo par achamar seu javascrito
