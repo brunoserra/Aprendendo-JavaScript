05214e63f7e78da9c35ec145ddffa8c8a839fd77
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-28 23:27:58 -0300

    subtração

--- learnjs/num-operadores.js
+++ learnjs/num-operadores.js
@@ -3,7 +3,15 @@ var operador = 100
 var operando = 50
 var resultado = operador * operando;
 console.log(resultado)
+
+//operadore string
 var nome = "bruno"
 var sobrenome = "serra"
 var nomecompleto = nome + " " + sobrenome;
 console.log(nomecompleto);
+
+//operador subtração
+var primeiroValor = 400
+var segundoValor = 20
+var resultados = primeiroValor - segundoValor;
+console.log(resultados);
