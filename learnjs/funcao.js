3bd368bc46c2b6f179f35df16191d7ddb9ef8d6d
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 02:21:53 -0300

    construindo

--- learnjs/funcao.js
+++ learnjs/funcao.js
@@ -3,7 +3,13 @@ function soma(operadorA, operadorB) {
     var resultadoC = operadorA + operadorB;
     return resultadoC;
 }
+
+function olaMundo(nome) {
+    console.log("ola mudo" + nome);
+
+}
 var resultadoSoma = soma(1, 2);
 var resultadoNovo = soma(120, 200);
 console.log(resultadoSoma);
 console.log(resultadoNovo);
+olaMundo("Bruno");
