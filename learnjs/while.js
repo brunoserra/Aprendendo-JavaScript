ba0797c9bebae8bce34e16c99906104965637347
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 01:37:06 -0300

    Create while.js

---
+++ learnjs/while.js
@@ -0,0 +1,13 @@
+//var tabuada = 7;
+//var repeticao = 10;
+
+//while (repeticao <= 10) {
+//var resultado = tabuada * repeticao;
+//console.log(resultado);}
+var achou = false;
+var numeroSorteado = 10;
+var possivelValores = 0;
+while (!achou) {
+    possivelValores = +1;
+
+}
