46743f94b578b384a5305548b20e58998c186477
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-28 23:22:45 -0300

    concatenar

--- learnjs/num-operadores.js
+++ learnjs/num-operadores.js
@@ -3,3 +3,7 @@ var operador = 100
 var operando = 50
 var resultado = operador * operando;
 console.log(resultado)
+var nome = "bruno"
+var sobrenome = "serra"
+var nomecompleto = nome + " " + sobrenome;
+console.log(nomecompleto);
