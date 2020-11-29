002fe9cc7e3dcf9cc2693cf212bf515574774e37
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-29 02:11:31 -0300

    funcao soma

---
+++ learnjs/funcao.js
@@ -0,0 +1,7 @@
+// funcao armazena bloco de codigos pode ser reutilziado
+function soma(operadorA, operadorB) {
+    var resultadoC = operadorA + operadorB;
+    return resultadoC;
+}
+var resultadoSoma = soma(1, 2);
+console.log(resultadoSoma);
